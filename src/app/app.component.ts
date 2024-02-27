b06e05cf58d2acc4f438003a9803f59a4a3d222d
Pushpanjali <chaurasiyapushpanjali256@gmail.com>
2024-02-28 00:06:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'owd';
-}
+
+  // iname="pihu";
+  uname="";
+  // ucolor="red";
+
+  but(val:any){
+    this.uname=val
+    console.log(val)
+  }
+  //  ubg="yellow";
+//    but(){
+//     alert('hello guys')
+//    }
+//   apply="a b c d";
+ }
